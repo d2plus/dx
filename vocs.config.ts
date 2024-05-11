56beac0f71f041063244542a7479fb131b619b78
network <net@d2.plus>
2024-05-11 19:28:40 +0700

    pnpm create vocs

---
+++ vocs.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vocs'
+
+export default defineConfig({
+  title: 'd2.plus',
+  sidebar: [
+    {
+      text: 'Getting Started',
+      link: '/getting-started',
+    },
+    {
+      text: 'Ubiquitous Language',
+      link: '/ubiquitous-language',
+    },
+  ],
+})
